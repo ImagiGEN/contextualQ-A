cb53cedbae0e32774f0afe04c82cdc2a76e1314c
Ashritha Goramane <ashritha.goramane@gmail.com>
2023-06-07 16:20:18 -0400

    Updated UI to call register user

--- ui/pages/1_User_Registration.py
+++ ui/pages/1_User_Registration.py
@@ -1,14 +1,19 @@
 import streamlit as st
+from utils import backend_api
+
 
 def register_user():
-    # Call Fast API with username and password field
-    pass
+    response = backend_api.register_user(new_user, new_passwd, new_cnf_passwd)
+    if response.json().get("username"):
+        st.write("User registered successfully!")
+    else:
+        st.write(response.json())
 
 st.title('User Registration')
 
 st.subheader("Create an Account")
 
 new_user = st.text_input('Username')
-new_passwd = st.text_input('Password',type='password')
-new_cnf_passwd = st.text_input('Confirm Password',type='password')
+new_passwd = st.text_input('Password', type='password')
+new_cnf_passwd = st.text_input('Confirm Password', type='password')
 st.button("Sign Up", on_click=register_user)
---
+++ ui/utils/backend_api.py
@@ -0,0 +1,21 @@
+import json
+import requests
+import os
+
+BACKEND_API_URL = os.getenv("BACKEND_API_URL", "http://api:8095")
+
+
+headers = {'Content-Type': 'application/json'}
+
+
+def register_user(username, password, confirm_password):
+    url = f"{BACKEND_API_URL}/api/v1/user/register"
+    payload = {
+        "username": username,
+        "password": password,
+        "cnf_password": confirm_password
+    }
+    json_payload = json.dumps(payload)
+
+    response = requests.request("POST", url, headers=headers, data=json_payload)
+    return response
